36644c70fe3cccbb943b109228a86ddd6d1587fb
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 05:30:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/chat/index.php'

--- app/view/chat/index.php
+++ app/view/chat/index.php
@@ -6,14 +6,20 @@
     <form id="form_msg" class="conversation-compose">
         <?php if (isset($_SESSION['id'])) { ?>
             <input id="mensagem" class="input-msg" name="input" placeholder="Escreva uma mensagem..." autocomplete="off" autofocus>
+            <button type="submit" class="send" onclick="playSound()">
+                <div class="circle">
+                    <i class="zmdi zmdi-mail-send"></i>
+                </div>
+            </button>
         <?php } else { ?>
-            <input id="mensagem" class="input-msg" name="input" placeholder="Escreva uma mensagem..." autocomplete="off" autofocus>  
+            <input id="usuario" placeholder="Usuário" autofocus>  
+            <input id="senha" placeholder="Senha">  
+            <button class="send" onclick="playSound()">
+                <div class="circle">
+                    <i class="zmdi zmdi-mail-send"></i>
+                </div>
+            </button>
         <?php } ?>
-        <button type="submit" class="send" onclick="playSound()">
-            <div class="circle">
-                <i class="zmdi zmdi-mail-send"></i>
-            </div>
-        </button>
     </form>
   </div>
 <!-- </div> -->
