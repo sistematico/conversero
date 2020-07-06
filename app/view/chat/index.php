2d960cc6c94189f0083ccc5b36f02b439285a558
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 02:55:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/chat/index.php'

--- app/view/chat/index.php
+++ app/view/chat/index.php
@@ -1,26 +1,6 @@
 <div class="page">
-  <div class="marvel-device nexus5">
-    <div class="top-bar"></div>
-    <div class="sleep"></div>
-    <div class="volume"></div>
-    <div class="camera"></div>
     <div class="screen">
       <div class="screen-container">
-        <div class="status-bar">
-          <div class="time"></div>
-          <div class="battery">
-            <i class="zmdi zmdi-battery"></i>
-          </div>
-          <div class="network">
-            <i class="zmdi zmdi-network"></i>
-          </div>
-          <div class="wifi">
-            <i class="zmdi zmdi-wifi-alt-2"></i>
-          </div>
-          <div class="star">
-            <i class="zmdi zmdi-star"></i>
-          </div>
-        </div>
         <div class="chat">
           <div class="chat-container">
             <div class="user-bar">
@@ -87,5 +67,4 @@
         </div>
       </div>
     </div>
-  </div>
 </div>
