1c621d6fb8419aab810031df2fbe7695553d536b
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 06:02:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/chat/index.php'

--- app/view/chat/index.php
+++ app/view/chat/index.php
@@ -1,6 +1,37 @@
 
 
-<!-- <div class="chat-container"> -->
+<div class="chat-container">
+
+
+
+
+
+<div class="user-bar">
+              <div class="back">
+                <i class="zmdi zmdi-arrow-left"></i>
+              </div>
+              <div class="avatar">
+                <img src="https://i.ibb.co/2Yg7tWv/Rumbiiha-Swaibu.jpg" alt="Avatar">
+              </div>
+              <div class="name">
+                <span>Rumbiiha s.</span>
+                <span class="status">online</span>
+              </div>
+              <div class="actions more">
+                <i class="zmdi zmdi-more-vert"></i>
+              </div>
+              <div class="actions attachment">
+                <i class="zmdi zmdi-phone"></i>
+              </div>
+              <div class="actions">
+                <img src="https://i.ibb.co/LdnbHSG/ic-action-videocall.png"/>             
+              </div>
+            </div>
+
+
+
+
+
   <div class="conversation">
     <div id="mensagens" class="conversation-container"></div>
         <form id="form_msg" class="conversation-compose" style="display:none">
