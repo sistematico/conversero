44e087dd5669c5162baf7c3b38eaa84ee5ca9126
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 07:03:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/Model/User.php'

--- app/Model/User.php
+++ app/Model/User.php
@@ -14,7 +14,7 @@ class User extends Model
 
     public function login($nome, $senha)
     {
-        if ($usuario = $this->get($nome) !== false) {
+        if ($usuario = $this->get($nome) != false) {
             if ($usuario->nome === $nome) {
                 $_SESSION['id'] = $usuario->id;
                 return json_encode(['id'=>$usuario->id,'nome'=>$usuario->nome]);
