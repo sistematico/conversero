36a09b0871690d02b6fcb97e90d0766a66519bf4
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 01:59:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/_templates/footer.php'

--- app/view/_templates/footer.php
+++ app/view/_templates/footer.php
@@ -1,5 +1,4 @@
 <script src="<?php echo URL; ?>js/jquery.min.js"></script>
-<script src="<?php echo URL; ?>js/bootstrap.bundle.min.js"></script>
 <script>var url = "<?php echo URL; ?>";</script>
 <script src="<?php echo URL; ?>js/application.js"></script>
 </body>
