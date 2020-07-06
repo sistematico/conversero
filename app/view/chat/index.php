6f2bed966d2e058ed10ec5e18fbe749c27ae2a3e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 02:51:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/_templates/header.php' add 'app/view/chat/index.php' add 'public/css/main.css' add 'public/css/main.old.css'

--- app/view/chat/index.php
+++ app/view/chat/index.php
@@ -1,19 +1,3 @@
-<footer class="footer">
-    <div class="container">
-    <form id="formmensagens" class="form-inline" action="<?php echo URL; ?>chat/index" method="post">
-        <div class="form-group row">
-            <div class="col-11">
-                <input id="mensagem" name="mensagem" type="text" class="form-control" placeholder="Diga um olá!" minlength="1" maxlength="100">
-            </div>
-            <div class="col-1">
-                <button id="envia" name="send_text" type="submit" class="btn btn-primary rounded m-auto"><i class="fas fa-paper-plane"></i></button>
-            </div>
-        </div>
-    </form>
-    </div>
-</footer>
-
-
 <div class="page">
   <div class="marvel-device nexus5">
     <div class="top-bar"></div>
