4563cbc7fe3dbdd48c6ebd69cd83b04ba84208ed
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 05:26:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/chat/index.php' add 'db/chat.sqlite'

--- app/view/chat/index.php
+++ app/view/chat/index.php
@@ -1,29 +1,22 @@
 
 
-<div class="chat-container">
+<!-- <div class="chat-container"> -->
   <div class="conversation">
-    <div id="mensagens" class="conversation-container">
-      <!-- <div class="message sent">
-        Mensagem
-        <span class="metadata">
-            <span class="time"></span><span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span>
-        </span>
-      </div>
-      <div class="message received">
-        Mensagem
-        <span class="metadata"><span class="time"></span></span>
-      </div> -->
-    </div>
+    <div id="mensagens" class="conversation-container"></div>
     <form id="form_msg" class="conversation-compose">
-      <input id="mensagem" class="input-msg" name="input" placeholder="Escreva uma mensagem..." autocomplete="off" autofocus>
-      <button type="submit" class="send" onclick="playSound()">
-          <div class="circle">
-            <i class="zmdi zmdi-mail-send"></i>
-          </div>
+        <?php if (isset($_SESSION['id'])) { ?>
+            <input id="mensagem" class="input-msg" name="input" placeholder="Escreva uma mensagem..." autocomplete="off" autofocus>
+        <?php } else { ?>
+            <input id="mensagem" class="input-msg" name="input" placeholder="Escreva uma mensagem..." autocomplete="off" autofocus>  
+        <?php } ?>
+        <button type="submit" class="send" onclick="playSound()">
+            <div class="circle">
+                <i class="zmdi zmdi-mail-send"></i>
+            </div>
         </button>
     </form>
   </div>
-</div>
+<!-- </div> -->
 
 
 
