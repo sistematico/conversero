db1f0bece5d279af6a7f82399ca8ff9f4d09b764
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 05:59:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/chat/index.php'

--- app/view/chat/index.php
+++ app/view/chat/index.php
@@ -3,17 +3,16 @@
 <!-- <div class="chat-container"> -->
   <div class="conversation">
     <div id="mensagens" class="conversation-container"></div>
-        <?php if (isset($_SESSION['id'])) { ?>
-            <form id="form_msg" class="conversation-compose">
-                <input id="mensagem" class="input-msg" name="input" placeholder="Escreva uma mensagem..." autocomplete="off" autofocus>
-                <button type="submit" class="send" onclick="playSound()">
-                    <div class="circle">
-                        <i class="zmdi zmdi-mail-send"></i>
-                    </div>
-                </button>
-            </form>
-        <?php } else { ?>
-            <form id="cadastroform" class="conversation-compose" style="display:none">
+        <form id="form_msg" class="conversation-compose" style="display:none">
+            <input id="mensagem" class="input-msg" name="input" placeholder="Escreva uma mensagem..." autocomplete="off" autofocus>
+            <button type="submit" class="send" onclick="playSound()">
+                <div class="circle">
+                    <i class="zmdi zmdi-mail-send"></i>
+                </div>
+            </button>
+        </form>
+
+        <form id="cadastroform" class="conversation-compose" style="display:none">
             <span id="loginlink">Login</span>
             <input id="usuario_cadastro" placeholder="Usuário" autofocus>  
             <input id="senha_cadastro" placeholder="Senha">  
@@ -22,9 +21,9 @@
                     <i class="zmdi zmdi-mail-send"></i>
                 </div>
             </button>
-            </form>
+        </form>
 
-            <form id="loginform" class="conversation-compose">
+        <form id="loginform" class="conversation-compose">
             <span id="cadastrolink">Cadastro</span>
             <input id="usuario_login" placeholder="Usuário" autofocus>  
             <input id="senha_login" placeholder="Senha">  
@@ -33,8 +32,7 @@
                     <i class="zmdi zmdi-mail-send"></i>
                 </div>
             </button>
-            </form>
-        <?php } ?>
+        </form>
   </div>
 <!-- </div> -->
 
