d6ed38af933e88c15a125a54832ff4c8103a033f
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 02:23:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/_templates/footer.php' add 'public/js/moment.min.js'

--- app/view/_templates/footer.php
+++ app/view/_templates/footer.php
@@ -1,4 +1,5 @@
 <script src="<?php echo URL; ?>js/jquery.min.js"></script>
+<script src="<?php echo URL; ?>js/moment.min.js"></script>
 <script>var url = "<?php echo URL; ?>";</script>
 <script src="<?php echo URL; ?>js/application.js"></script>
 </body>
