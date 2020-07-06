ec3bd113dc21d22a65585034c0d4bdc914a18e10
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-06 02:53:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/_templates/header.php'

--- app/view/_templates/header.php
+++ app/view/_templates/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Conversero</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.1.2/css/material-design-iconic-font.min.css">
-    <link rel="stylesheet" href="<?php echo URL; ?>css/main.css">
+    <link rel="stylesheet" href="<?php echo URL; ?>css/main.css?v=<?php echo uniqid(); ?>">
     <link rel="shortcut icon" href="<?php echo URL; ?>img/favicon.ico">
 </head>
 <body>
